670b9a37b9f58086fa9d55bdda1a958bc61ad19c
techwizTri <tri33hoang@gmail.com>
2023-07-09 16:07:18 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '21b2c775-bd06-468f-b163-05c7ec3c2b44';
+export const MODAL_CLOSE_SEC = 2.5;
